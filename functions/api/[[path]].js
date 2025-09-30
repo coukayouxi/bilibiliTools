4e39322f675307ce5f085be1f09be95610ce846c
coukayouxi <3127500373@qq.com>
2025-09-30 22:55:26 +0800

    3
    3

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -11,6 +11,22 @@ export const onRequestOptions = async () => {
 
 // 处理所有其他请求，添加CORS头
 export const onRequest = async (context) => {
+  // 获取请求路径
+  const url = new URL(context.request.url);
+  const path = url.pathname;
+  
+  // 如果路径以 /api/user-, /api/video-, /api/live- 等开头，重定向到主页并添加hash
+  if (path.startsWith('/api/user-')) {
+    const uid = path.substring(10); // 提取 uid
+    return Response.redirect(url.origin + '/#user-' + uid, 301);
+  } else if (path.startsWith('/api/video-')) {
+    const aid = path.substring(11); // 提取 aid
+    return Response.redirect(url.origin + '/#video-' + aid, 301);
+  } else if (path.startsWith('/api/live-')) {
+    const room = path.substring(10); // 提取 room
+    return Response.redirect(url.origin + '/#live-' + room, 301);
+  }
+  
   const response = await context.next();
   
   response.headers.set('Access-Control-Allow-Origin', '*');
